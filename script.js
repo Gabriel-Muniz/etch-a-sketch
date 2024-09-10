d19b5d98f8ff6dae47c1c9eac98ccd2661ee479b
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-09-09 21:44:18 -0300

    Feat: create grid

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+const contentContainer = document.querySelector(".content");
+
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container");
+
+for (let i = 0; i < 16; i++) {
+  const gridLine = document.createElement("div");
+  gridLine.classList.add("grid-line");
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    gridLine.appendChild(cell);
+  }
+  gridContainer.appendChild(gridLine);
+}
+
+contentContainer.appendChild(gridContainer);
