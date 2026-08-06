63191c545b911bc99335f94e2c0c83ce9ba8396f
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-06 14:29:41 -0300

    feat: grid create function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+function makeGrid(gridDimensions) {
+    const gridContainer = document.querySelector('.grid-container');
+
+    gridContainer.innerHTML = '';
+
+    for (let i = 0; i < gridDimensions; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+
+        for (let j = 0; j < gridDimensions; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cell');
+
+            gridRow.appendChild(gridCell);
+        }
+
+        gridContainer.appendChild(gridRow);
+
+    }
+
+}
+
+makeGrid(16);
