d36a464d8569cc0ae1ef625d55466fb5dc449db0
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-06 16:32:43 -0300

    feat: random color mode
    colors are chosen from a array and randomized before being painted

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const gridContainer = document.querySelector('.grid-container');
 const resizeBtn = document.querySelector('.resize-grid-btn');
+const randomBtn = document.querySelector('.random-color-btn');
+
+let randomMode = false;
+const COLORS_RANDOM = [
+    '#000',
+    '#585858',
+    '#c0c0c0',
+    '#f0f0f0',
+    '#b52',
+    '#fd0',
+    '#cf0',
+    '#790',
+    '#0dd',
+    '#80f',
+    '#f0d',
+    '#f05',
+    '#ffc',
+    '#bb9'
+]
+
+randomBtn.addEventListener('click', (e) => {
+    const spanState = document.querySelector('.btn-state');
+
+    randomMode = !randomMode;
+
+    spanState.classList.toggle('state-on')
+
+
+    spanState.textContent = (randomMode) ? 'ON' : "OFF";
+})
 
 resizeBtn.addEventListener('click', (e) => {
     let newGridSize = prompt(`Insert size you want your grid:\nObs: Should be between 1 and 100`);
     const VALID__GRID_SIZE = (newGridSize > 0 && newGridSize <= 100) ? true : false;
 
-    if(VALID__GRID_SIZE) {
+    if (VALID__GRID_SIZE) {
         makeGrid(newGridSize);
         return
     }
@@ -17,6 +47,11 @@ resizeBtn.addEventListener('click', (e) => {
 
 gridContainer.addEventListener('mouseover', (e) => {
     e.target.classList.add('painted');
+
+    if (randomMode) {
+        let colorIndex = Math.floor(Math.random() * COLORS_RANDOM.length + 1);
+        e.target.style.backgroundColor = COLORS_RANDOM[colorIndex];
+    }
 })
 
 function makeGrid(gridDimensions = 16) {
